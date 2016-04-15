fbdc94235391e6e5b802ad278609dba5118253f6
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-04-15 09:50:43 -0500

    adding an exercise to folder

---
+++ strings-continued.php
@@ -0,0 +1,17 @@
+<?php
+
+$song = "We don't need no education \nWe don't need no thought control.\n";
+echo "$song\n";
+
+$song2 = "All the World's a stage\nAnd all the men and women merely players:\nThey have their exits and their entrances;";
+echo "$song2\n";
+
+$firstName = 'Joe';
+$lastName = 'Blow';
+$address = '123 Any Street';
+
+echo "$lastName, $firstName, $address\n";
+
+echo "$firstName$lastName$address\n";
+
+echo "$firstName $lastName\n$address\n";
