b820088756a5094a5a1bcdb9cc9de1ea18e0d37a
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-04-18 15:05:00 -0500

    - Constructed a loop that iterates through each value and outputs its type as either integer, float, boolean, array, null, or string. step 2

--- foreach.php
+++ foreach.php
@@ -1,3 +1,25 @@
 <?php
 
-foreach.php exercise
+
+
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
+foreach ($things as $stuff) {
+
+    if (is_numeric($stuff)) {
+        echo "{$stuff} is a number\n";
+    }
+    elseif (is_string($stuff)) { 
+        echo "{$stuff} is a string\n";
+    }
+    elseif (is_bool($stuff)) {
+        echo "{$stuff} is a boolean\n";
+    }
+    elseif (is_null($stuff)) {
+        echo "{$stuff} is a null\n";
+    }
+    elseif (is_array($stuff)) {
+        echo "{$stuff} is an array\n";
+    }
+
+}
