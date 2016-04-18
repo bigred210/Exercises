4fdb941c8d2489d100f6ceb918de3e28c7558ce1
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-04-18 14:25:20 -0500

    added say to for loop on output

---
+++ fizzbuzz.php
@@ -0,0 +1,41 @@
+<?php
+
+// for ($i = 1; $i <= 100; $i++) {
+
+
+//     if ( ($i % 3 == 0 ) && ($i % 5 == 0) ) {
+//         echo "FIZZBUZZ\n";
+//     }
+//     elseif ($i % 3 == 0) {
+//         echo "FIZZ\n";
+//     }
+//     elseif ($i % 5 == 0) {
+//         echo "BUZZ\n";
+    
+//     }else {
+//         echo "$i\n";
+//     }
+
+// }
+
+for ($i = 1; $i <= 100; $i++) {
+
+
+    if ( ($i % 3 == 0 ) && ($i % 5 == 0) ) {
+        `say -v "victoria" FIZZBUZZ\n`;
+        echo "FIZZBUZZ\n";
+    }
+    elseif ($i % 3 == 0) {
+        `say -v "victoria" FIZZ\n`;
+        echo "FIZZ\n";
+    }
+    elseif ($i % 5 == 0) {
+        `say -v "victoria" BUZZ\n`;
+        echo "BUZZ\n";
+    
+    }else {
+        `say -v "victoria" {$i}\n`;
+        echo "$i\n";
+    }
+
+}
