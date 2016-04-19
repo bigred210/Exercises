652dbbfd4324c58b15348764e350a72eeecaa664
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-04-19 15:37:22 -0500

    Validate all the arguments, and display an error if the input is not numeric. step1

--- arithmetic.php
+++ arithmetic.php
@@ -2,34 +2,62 @@
 
 function add($a, $b)
 {
-    return $a + $b;
+    if (is_numeric($a) && is_numeric($b)) {
+        return $a + $b;
+    } else {
+        return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
 function subtract($a, $b)
 {
-    // Add code here
-    return $a - $b;
+    if (is_numeric($a) && is_numeric($b)) {
+        return $a - $b;
+    } else {
+        return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
 function multiply($a, $b)
 {
-    // Add code here
-    return $a * $b;
+    if (is_numeric($a) && is_numeric($b)) {
+        return $a * $b;
+    } else {
+        return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
 function divide($a, $b)
 {
-    // Add code here
-    return $a / $b;
+    if (is_numeric($a) && is_numeric($b)) {
+        return $a / $b;
+    } else {
+        return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
-// Add code to test your functions here
+
 echo add(5, 2) . PHP_EOL;
-echo subtract(5, 2) . PHP_EOL;
-echo multiply(5, 2) . PHP_EOL;
-echo divide(5, 2) . PHP_EOL;
 echo "----------\n";
+echo subtract(5, 2) . PHP_EOL;
+echo "----------\n";
+echo multiply(5, 2) . PHP_EOL;
+echo "----------\n";
+echo divide(5, 2) . PHP_EOL;
+
+echo "----------\n";
+echo "----------\n";
+echo "----------\n";
+
 echo add(5, 2) . PHP_EOL;
+echo "----------\n";
 echo add(5, -2) . PHP_EOL;
+echo "----------\n";
 echo add(5, 0) * add(2, 0) . PHP_EOL;
+echo "----------\n";
 echo add(5, 0) / add(2, 0) . PHP_EOL;
+
+
+
+
+
