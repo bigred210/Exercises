34ba7b19bf38d7ca94bb595a2822364f7196c352
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-04-19 14:20:56 -0500

    added code within each function ( return $a (?) $b;)

---
+++ arithmetic.php
@@ -0,0 +1,27 @@
+<?php
+
+function add($a, $b)
+{
+    return $a + $b;
+}
+
+function subtract($a, $b)
+{
+    // Add code here
+    return $a - $b;
+}
+
+function multiply($a, $b)
+{
+    // Add code here
+    return $a * $b;
+}
+
+function divide($a, $b)
+{
+    // Add code here
+    return $a / $b;
+}
+
+// Add code to test your functions here
+
