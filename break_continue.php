8e040129c4870eab358aa6e6c76efebd25070e20
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-04-19 11:14:18 -0500

    fixed for loop on step 1 to use continue

--- break_continue.php
+++ break_continue.php
@@ -1,11 +1,10 @@
 <?php
 
 for ($i = 1; $i <= 100; $i++) {
-    if ($i % 2 == 0 )  {
-        echo $i . "\n";
-        continue;
-        
+    if ($i % 2 == 1)  {
+        continue;   
     }
+    echo "$i \n";
 }
 
 for ($i = 1; $i <= 100; $i++) {
