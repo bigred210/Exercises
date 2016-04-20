2ea4048bcab22b21c85170e2930ceefeece44b1b
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-04-20 08:20:46 -0500

    finished the steps and cleaned up spacing on code

--- arithmetic.php
+++ arithmetic.php
@@ -2,59 +2,80 @@
 
 function add($a, $b)
 {
-    if (is_numeric($a) && is_numeric($b)) {
-        return $a + $b;
+    if (!is_numeric($a) || !is_numeric($b)) {
+        return throwErrorMessage($a, $b);
     } else {
-        return "ERROR: Both arguments {$a} and {$b}  must be numbers\n";
+        return $a + $b;
     }
 }
 
+
 function subtract($a, $b)
 {
-    if (is_numeric($a) && is_numeric($b)) {
-        return $a - $b;
+    if (!is_numeric($a) || !is_numeric($b)) {
+        return throwErrorMessage($a, $b);
     } else {
-        return "ERROR: Both arguments {$a} and {$b} must be numbers\n";
+        return $a - $b;
     }
 }
 
 function multiply($a, $b)
 {
-    if (is_numeric($a) && is_numeric($b)) {
-        return $a * $b;
+    if (!is_numeric($a) || !is_numeric($b)) {
+        return throwErrorMessage($a, $b);
     } else {
-        return "ERROR: Both arguments {$a} and {$b} must be numbers\n";
+        return $a * $b;
     }
 }
 
 function divide($a, $b)
 {
-   if (is_numeric($a) && is_numeric($b) && ($b != 0)) {
-        return $a / $b;
+   if (!is_numeric($a) || !is_numeric($b) || ($b == 0)) {
+        return throwErrorMessage2($a, $b);
     } else {
-        return "ERROR: Both arguments {$a} and {$b} must be numbers or you are trying to divide by zer\n";
+         return $a / $b;
     }
 }
 
+function throwErrorMessage($a, $b) 
+{
+    echo "ERROR: Both arguments \$a : {$a} and \$b : {$b} must be numbers\n";
+}
+function throwErrorMessage2($a, $b) 
+{
+    echo "ERROR: Both arguments \$a : {$a} and \$b : {$b} must be numbers or you are trying to divide by zero\n";
+}
 
 echo add(5, 2) . PHP_EOL;
-echo "----------\n";
-echo subtract(5, 2) . PHP_EOL;
-echo "----------\n";
+
+echo "---\n";
+
+echo subtract(5, "fdkfd") . PHP_EOL;
+
+echo "---\n";
+
 echo multiply(5, 2) . PHP_EOL;
-echo "----------\n";
+
+echo "---\n";
+
 echo divide(5, 2) . PHP_EOL;
 
-echo "----------\n";
-echo "--++++++--\n";
-echo "----------\n";
+
+echo "__ ʕ•̫͡•ʕ*̫͡*ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ*̫͡*ʔ-̫͡-ʔ __\n"; 
+
 
 echo add(5, 2) . PHP_EOL;
-echo "----------\n";
+
+echo "---\n";
+
 echo add(5, -2) . PHP_EOL;
-echo "----------\n";
+
+echo "---\n";
+
 echo add(5, 0) * add(2, 0) . PHP_EOL;
-echo "----------\n";
+
+echo "---\n";
+
 echo add(5, 0) / add(2, 0) . PHP_EOL;
 
 
