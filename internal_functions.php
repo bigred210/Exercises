8785d5fc59f5aeb674fc401d534e7b8f8225dc3e
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-04-20 09:45:28 -0500

    added inspect function and if statements

--- internal_functions.php
+++ internal_functions.php
@@ -1,7 +1,24 @@
 <?php
 
 // TODO: Create your inspect() function here
-
+function inspect($variable) 
+{
+    if (is_string($variable)) {
+        return " I am a String\n";
+    }elseif (is_array($variable)) {
+        return " I am an Array\n";
+    }elseif (is_bool($variable)) {
+        return " I am a Boolean\n";
+    }elseif (is_int($variable)) {
+        return " I am an Integer\n";
+    }elseif (is_float($variable)) {
+        return " I am a Float\n";
+    }elseif (is_null($variable)) {
+        return " I am NULL\n";
+    }else {
+        return " I am having an identity crisis\n";
+    }
+}
 
 // Do not mofify these variables!
 $string1 = "I'm a little teapot";
@@ -11,10 +28,11 @@ $array2 = [1, 2, 3];
 $bool1 = true;
 $bool2 = false;
 $num1 = 0;
-$num2 = 0.0;
+$num2 = 
 $num3 = 12;
 $num4 = 14.4;
 $null = NULL;
+
 
 
 // TODO: After each echo statement, use inspect() to output the variable's type and its value
