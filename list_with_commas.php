9c704cc52ced15e6b4ecb9d8d214a5549210bdd3
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-04-21 14:42:48 -0500

    step2 complete

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
 $physicistsArray = explode(', ', $physicistsString);
@@ -15,3 +14,27 @@ $newPhysicistsString .= ' and ' . $lastPhysicist;
 $famousFakePhysicists = $newPhysicistsString;
 
 echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}." . PHP_EOL;
+
+echo "===================================\n";
+
+function humanizedList($physicistsArray) {
+
+    $lastPhysicist = array_pop($physicistsArray);
+
+    $newPhysicistsString = implode(', ', $physicistsArray);
+
+    $famousFakePhysicists = $newPhysicistsString .= ' and ' . $lastPhysicist;
+
+        return $famousFakePhysicists;
+}
+
+humanizedList($physicistsArray);
+
+echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}." . PHP_EOL;
+
+ 
+
+
+
+
+
