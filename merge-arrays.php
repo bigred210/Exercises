ce095d05b8e91d866a520438556d957f50a5f5a3
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-04-21 09:50:05 -0500

    step1 complete step2 started     - have the function built and it returns names       but need to tweak a few things within it to       get the exercises expected outcome.

---
+++ merge-arrays.php
@@ -0,0 +1,74 @@
+<?php
+
+$names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
+
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+
+
+
+echo "------------------------\n";
+
+function searchingArrays($query, $anyArray) {
+    if (array_search($query, $anyArray) !== false) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+echo searchingArrays('Tina', $names) . PHP_EOL;
+
+
+echo "------------------------\n";
+echo "------------------------\n";
+
+
+$arrayOne = $names;
+$arrayTwo = $compare;
+
+function inCommon($arrayOne, $arrayTwo) {
+    $namesCommon = 0;
+    foreach ($arrayOne as $query) {
+        if (searchingArrays($query, $arrayTwo) == true) {
+            $namesCommon++;
+        }
+    }  
+    return $namesCommon; 
+}
+
+echo inCommon($arrayOne, $arrayTwo) . PHP_EOL;
+
+
+echo "------------------------\n";
+echo "------------------------\n";
+
+
+$arrayOne = $names;
+$arrayTwo = $compare;
+
+
+function combine_arrays($arrayOne, $arrayTwo) {
+    $arrayCombine = [];
+    foreach ($arrayOne as $query) {
+        if (searchingArrays($query, $arrayTwo) == true) {
+            array_unshift($arrayCombine, $query);
+        } elseif (searchingArrays($query, $arrayTwo) == false) {
+            array_push($arrayCombine, $query);
+        }
+    }
+    return $arrayCombine;
+}
+var_dump(combine_arrays($arrayOne, $arrayTwo));
+
+
+echo "------------------------\n";
+
+
+
+
+
+
+
+
+
