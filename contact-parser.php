78b7b73b6dcf8fe6902b4ac0687dff81c6211c22
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-04-22 14:45:26 -0500

    opened to view file change $ name on something.

--- contact-parser.php
+++ contact-parser.php
@@ -9,18 +9,19 @@ function parseContacts($filename)
     array_pop($contactsArray); 
     fclose($handle);
 
+        // foreach created to loop through $contentsArray 
     foreach ($contactsArray as $friends) {
-           
+            // new empty array created to push info in
         $contactList = [];
-
+            // var equals info split at the | in $friends
         $personList = explode("|", $friends);
-
+            // var equals info at index[0] for $personList 
         $name = $personList[0];
-
+            // var equals info at index[1] for $personList * using subtr to concat a "-" 
         $phnNumber = substr($personList[1], 0, 3) . "-" . substr($personList[1], 3, 3) . "-" . substr($personList[1], 6, 4);
-
+            // setting empty array $contactList to equal [Name[0] and Number[1]
         $contactList = ["Name" => $name , "Number" => $phnNumber];
-
+            // pushing info of $contactList into $contentsArray 
         array_push($contentsArray, $contactList);
     }
 
