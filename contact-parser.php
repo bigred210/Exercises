fbc584b8a8050fa9ca5b51f14e73d0004226afad
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-04-22 10:44:09 -0500

    finished steps

--- contact-parser.php
+++ contact-parser.php
@@ -2,12 +2,29 @@
 
 function parseContacts($filename)
 {
+    $contentsArray = [];
     $handle = fopen($filename, 'r');
     $contents = fread($handle, filesize($filename));
     $contactsArray = explode("\n", $contents);
-    fclose($handle);
     array_pop($contactsArray); 
-    return $contactsArray;
+    fclose($handle);
+
+    foreach ($contactsArray as $friends) {
+           
+        $contactList = [];
+
+        $personList = explode("|", $friends);
+
+        $name = $personList[0];
+
+        $phnNumber = substr($personList[1], 0, 3) . "-" . substr($personList[1], 3, 3) . "-" . substr($personList[1], 6, 4);
+
+        $contactList = ["Name" => $name , "Number" => $phnNumber];
+
+        array_push($contentsArray, $contactList);
+    }
+
+    return $contentsArray;
 }
 
 var_dump(parseContacts('contacts.txt'));
