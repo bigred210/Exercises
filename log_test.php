36aec9dee77a95cb86a944dc7ab144ca0dc46459
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-05-02 14:24:10 -0500

    completed exercises for construct/deconstruct

--- log_test.php
+++ log_test.php
@@ -5,9 +5,8 @@ require_once 'Log.php';
 date_default_timezone_set("America/Chicago");
 
 $log = new Log;
-$log ->filename = 'Jerod';
 
 
 $log->logInfo("Yay! Info stuff."); 
 
-// $log->logError("OOPs! Error Error Error.");
+$log->logError("OOPs! Error Error Error.");
